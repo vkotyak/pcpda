5e4ae05419c9dca90f45920ef082dbeb7cee1637
Vinga <v.kotyak@gmail.com>
2019-09-08 21:50:42 +0300

    styling

--- 1/view.php
+++ 1/view.php
@@ -3,28 +3,41 @@
 class View {
 
     public function makeForm(){
-        $html = '';
-        $html .= '<form action="" method="POST">
-        <select name="town">
-        <option value="-"> </option>
-        <option value="Кировоград">Кропивницький</option>
-        <option value="Днепропетровск">Дніпро</option>
-        </select>
-        <input type="submit" value="Знайти">
-        </form>';
+        $html = '<div class="row">';
+        $html .= '<form action="" method="POST" class="col s12">
+	<fieldset class="row">
+	<div class="input-field col s12">
+            <select name="town">
+                <option value="-"> </option>
+                <option value="Кировоград">Кропивницький</option>
+                <option value="Днепропетровск">Дніпро</option>
+            </select>
+	<label>Select town</label>
+	</div>
+	<div class="input-field col s12">
+	    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+	        <i class="material-icons right">send</i>
+	      </button>
+	</div>
+	</fieldset>
+        </form>
+	</div>';
         return $html;
     }
     public function makeResult($data,$town="") {
-        $html ='<h2>'.$town.'</h2>';
-        $html .= '<ul>';
+        $html = '<div class="row">';
+        $html .='<h2 class="header col s12">'.$town.'</h2>';
+        $html .= '<ul class="col s12 collection">';
         foreach ($data as $atm) {
-            $html .='<li>';
+            $html .='<li class="collection-item">';
             $html .=$atm['name'];
             $html .=' ';
             $html .=$atm['address'];
             $html .='</li>';
         }
         $html .='</ul>';
+        $html .='</div>';
+
         return $html;
     }
 }
